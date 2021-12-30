ad137e8d117e73e74b7683c5722cef35bcb9e620
Stefan Imhoff <stefan@imhoff.name>
2021-12-30 19:20:43 +0100

    fix: remove 100vh fix

--- postcss.config.js
+++ postcss.config.js
@@ -6,7 +6,6 @@ module.exports = {
     require('postcss-extend'),
     require('postcss-media-minmax'),
     require('postcss-custom-media'),
-    require('postcss-100vh-fix'),
     require('postcss-sort-media-queries')(),
     require('autoprefixer'),
     ...(process.env.ELEVENTY_ENV === 'production' ? [require('cssnano')] : []),
